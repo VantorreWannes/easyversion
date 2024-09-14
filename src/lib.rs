5507a7a878a2793b08e47aa799c17492ad238743
Wannes Vantorre <vantorrewannes@gmail.com>
2024-09-14 13:54:13 +0200

    feature: remove git2 mentions

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,8 @@
 #[cfg(test)]
 mod tests {
 
-    use git2::Repository;
-
     #[test]
-    fn create_new_repo() {
-        let path = "test/test_repo";
-        let repo = Repository::init(path).expect("Path is valid");
+    fn testing() {
+        todo!();
     }
 }
