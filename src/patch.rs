a5a16ee3ad834ca8c8d74a676df4ed1bc7d3bea7
Wannes Vantorre <vantorrewannes@gmail.com>
2024-09-16 12:43:07 +0200

    refactor: Rewrite patch::apply

--- src/patch.rs
+++ src/patch.rs
@@ -42,16 +42,41 @@ impl Patch {
 
     fn compress(data: &[u8]) -> io::Result<Vec<u8>> {
         let mut encoder = BzEncoder::new(data, Compression::best());
-        let mut compressed_data = Vec::new();
+        let mut compressed_data = vec![];
         encoder.read_to_end(&mut compressed_data)?;
         Ok(compressed_data)
     }
 
     fn decompress(data: &[u8]) -> io::Result<Vec<u8>> {
         let mut decoder = BzDecoder::new(data);
-        let mut decompressed_data = Vec::new();
+        let mut decompressed_data = vec![];
         decoder.read_to_end(&mut decompressed_data)?;
         Ok(decompressed_data)
+    }
+
+    pub fn apply_buffer(&self, source: &[u8]) -> io::Result<Vec<u8>> {
+        let mut patch = fs::read(&self.path)?;
+        patch = Self::decompress(&patch)?;
+        let mut target_buffer = vec![];
+        bsdiff::patch(source, &mut patch.as_slice(), &mut target_buffer)?;
+        Ok(target_buffer)
+    }
+
+    pub fn apply_to_file(&self, source_path: impl AsRef<Path>, target_path: impl AsRef<Path>) -> io::Result<()> {
+        let source = std::fs::read(&source_path)?;
+        let target = self.apply_buffer(&source)?;
+        std::fs::write(&target_path, target)?;
+        Ok(())
+    }
+
+    pub fn path(&self) -> &Path {
+        &self.path
+    }
+}
+
+impl From<Patch> for PathBuf {
+    fn from(patch: Patch) -> Self {
+        patch.path
     }
 }
 
@@ -63,5 +88,17 @@ mod patch_tests {
     fn test_patch() {
         let patch = Patch::new("test-data/patch/A.txt", "test-data/patch/B.txt", "test-data/patch");
         assert!(patch.is_ok());
+    }
+
+    #[test]
+    fn test_apply() -> io::Result<()> {
+        let source_path = "test-data/patch/A.txt";
+        let target_path = "test-data/patch/B.txt";
+        assert_eq!(fs::read_to_string(target_path)?, "ABCDEFG");
+        let patch = Patch::new(source_path, target_path, "test-data/patch")?;
+        fs::write(target_path, "")?;
+        patch.apply_to_file(source_path, target_path)?;
+        assert_eq!(fs::read_to_string(target_path)?, "ABCDEFG");
+        Ok(())
     }
 }
