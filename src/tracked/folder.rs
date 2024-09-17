acea18746ea8e104a263264c78315e47b4d96166
Wannes Vantorre <vantorrewannes@gmail.com>
2024-09-17 13:49:00 +0200

    feature: Move TrackedItem to general mod folder

--- src/tracked/folder.rs
+++ src/tracked/folder.rs
@@ -5,48 +5,7 @@ use std::{
 
 use serde::{Deserialize, Serialize};
 
-use super::file::TrackedFile;
-
-#[derive(Debug, PartialEq, Eq, Clone, Serialize, Deserialize)]
-pub enum TrackedItem {
-    File(TrackedFile),
-    Folder(TrackedFolder),
-}
-
-impl TrackedItem {
-    pub fn save(&mut self) -> io::Result<()> {
-        match self {
-            Self::File(file) => file.save(),
-            Self::Folder(folder) => folder.save(),
-        }
-    }
-
-    pub fn load(&mut self, index: usize) -> io::Result<()> {
-        match self {
-            Self::File(file) => file.load(index),
-            Self::Folder(folder) => folder.load(index),
-        }
-    }
-
-    pub fn delete(&mut self, index: usize) -> io::Result<()> {
-        match self {
-            Self::File(file) => file.delete(index),
-            Self::Folder(folder) => folder.delete(index),
-        }
-    }
-}
-
-impl From<TrackedFile> for TrackedItem {
-    fn from(item: TrackedFile) -> Self {
-        Self::File(item)
-    }
-}
-
-impl From<TrackedFolder> for TrackedItem {
-    fn from(item: TrackedFolder) -> Self {
-        Self::Folder(item)
-    }
-}
+use super::{file::TrackedFile, TrackedItem};
 
 #[derive(Debug, PartialEq, Eq, Clone, Serialize, Deserialize)]
 pub struct TrackedFolder {
--- src/tracked/mod.rs
+++ src/tracked/mod.rs
@@ -1,2 +1,49 @@
+use std::io;
+
+use file::TrackedFile;
+use folder::TrackedFolder;
+use serde::{Deserialize, Serialize};
+
 pub mod file;
 pub mod folder;
+
+#[derive(Debug, PartialEq, Eq, Clone, Serialize, Deserialize)]
+pub enum TrackedItem {
+    File(TrackedFile),
+    Folder(TrackedFolder),
+}
+
+impl TrackedItem {
+    pub fn save(&mut self) -> io::Result<()> {
+        match self {
+            Self::File(file) => file.save(),
+            Self::Folder(folder) => folder.save(),
+        }
+    }
+
+    pub fn load(&mut self, index: usize) -> io::Result<()> {
+        match self {
+            Self::File(file) => file.load(index),
+            Self::Folder(folder) => folder.load(index),
+        }
+    }
+
+    pub fn delete(&mut self, index: usize) -> io::Result<()> {
+        match self {
+            Self::File(file) => file.delete(index),
+            Self::Folder(folder) => folder.delete(index),
+        }
+    }
+}
+
+impl From<TrackedFile> for TrackedItem {
+    fn from(item: TrackedFile) -> Self {
+        Self::File(item)
+    }
+}
+
+impl From<TrackedFolder> for TrackedItem {
+    fn from(item: TrackedFolder) -> Self {
+        Self::Folder(item)
+    }
+}
