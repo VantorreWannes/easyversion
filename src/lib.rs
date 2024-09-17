0157e7601d2db10486b2217db9870f0c92ef87e1
Wannes Vantorre <vantorrewannes@gmail.com>
2024-09-17 19:02:21 +0200

    feature: Implement hash helper function

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,16 @@
+use core::hash::Hash;
+use std::hash::{DefaultHasher, Hasher};
+
+pub mod patch;
+
+pub fn hash<T>(value: T) -> u64
+where
+    T: Hash,
+{
+    let mut hasher = DefaultHasher::new();
+    value.hash(&mut hasher);
+    hasher.finish()
+}
 
 #[cfg(test)]
 mod tests {
