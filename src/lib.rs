d12b262d249c52d7f2a36212fdb2a5755b598e76
Wannes Vantorre <vantorrewannes@gmail.com>
2024-09-20 12:02:24 +0200

    del: Remove old versions of tracked types

--- src/lib.rs
+++ src/lib.rs
@@ -4,8 +4,8 @@ use std::hash::{DefaultHasher, Hasher};
 pub mod patch;
 pub mod timeline;
 pub mod tracked;
-pub mod tracked_file;
-pub mod tracked_folder;
+// pub mod tracked_file;
+// pub mod tracked_folder;
 
 pub fn hash<T>(value: T) -> u64
 where
