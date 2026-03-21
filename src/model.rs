ea2435cf8efdc9e229d9d714904fa7af52fe6b84
Wannes Vantorre <vantorrewannes@gmail.com>
2026-03-21 19:40:00 +0100

    Add Manifest, Snapshot, and History structs

--- src/model.rs
+++ src/model.rs
@@ -1,6 +1,24 @@
+use std::{collections::HashMap, path::PathBuf};
+
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, PartialEq, Eq, Clone, Copy, Hash, Serialize, Deserialize)]
 pub struct Id {
     pub digest: u64,
+}
+
+#[derive(Debug, PartialEq, Eq, Clone, Serialize, Deserialize)]
+pub struct Manifest {
+    pub files: HashMap<PathBuf, Id>,
+}
+
+#[derive(Debug, PartialEq, Eq, Clone, Serialize, Deserialize)]
+pub struct Snapshot {
+    pub comment: Option<String>,
+    pub manifest: Manifest,
+}
+
+#[derive(Debug, PartialEq, Eq, Clone, Serialize, Deserialize, Default)]
+pub struct History {
+    pub snapshots: Vec<Snapshot>,
 }
